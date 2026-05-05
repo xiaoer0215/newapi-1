16829b310a4c74ea86e65ccd0e94d704273ec73e
xiaoer0215 <113199679+xiaoer0215@users.noreply.github.com>
2026-05-05 13:14:57 +0800

    Update cache.go

--- middleware/cache.go
+++ middleware/cache.go
@@ -1,15 +1,20 @@
 package middleware
 
 import (
+	"path"
+
 	"github.com/gin-gonic/gin"
 )
 
 func Cache() func(c *gin.Context) {
 	return func(c *gin.Context) {
-		if c.Request.RequestURI == "/" {
-			c.Header("Cache-Control", "no-cache")
+		requestPath := c.Request.URL.Path
+		if path.Ext(requestPath) == "" {
+			c.Header("Cache-Control", "no-store, no-cache, must-revalidate, private, max-age=0")
+			c.Header("Pragma", "no-cache")
+			c.Header("Expires", "0")
 		} else {
-			c.Header("Cache-Control", "max-age=604800") // one week
+			c.Header("Cache-Control", "public, max-age=604800, immutable") // one week
 		}
 		c.Header("Cache-Version", "b688f2fb5be447c25e5aa3bd063087a83db32a288bf6a4f35f2d8db310e40b14")
 		c.Next()
